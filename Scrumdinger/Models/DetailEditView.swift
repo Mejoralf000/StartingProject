fe7c375df54105c59804792aa1022778931dab35
Mejoralf000 <mejoralf000@gmail.com>
2024-04-17 09:27:16 -0600

    added Detail view and made changes to Scrums and Edit view, dailyscrum, theme, and ScrumDingerApp

---
+++ Scrumdinger/Models/DetailEditView.swift
@@ -0,0 +1,50 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Alfredo Mejorado on 4/10/24.
+//
+
+import SwiftUI
+
+
+struct DetailEditView: View {
+    @State private var scrum = DailyScrum.emptyScrum
+    @State private var newAttendeeName = ""
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Meeting Info")) {
+                TextField("Title", text: $scrum.title)
+                HStack {
+                    Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
+                        Text("Length")
+                    }
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+            }
+            Section (header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                .onDelete { indices in
+                    scrum.attendees.remove(atOffsets: indices)
+                }
+                HStack {
+                    TextField("New Attendee", text: $newAttendeeName)
+                    Button(action: {}) {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -7,14 +7,26 @@
 
 import SwiftUI
 
+
 struct ScrumsView: View {
     let scrums: [DailyScrum]
     
     var body: some View {
-        List(scrums, id: \.title) { scrum in
-
-
+        NavigationStack{
+            List(scrums) { scrum in
+                NavigationLink(destination: DetailView(scrum: scrum)) {
+                    CardView(scrum: scrum)
                 }
+                .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
+            }
+        }
     }
 }
 
